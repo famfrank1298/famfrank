c9298f0651ab708e87c8e08234f126d4641efdad
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-07-20 15:29:50 -0500

    v2 responsive (no links yet)

--- src/components/ui/NavBar/navbar.tsx
+++ src/components/ui/NavBar/navbar.tsx
@@ -1,19 +1,36 @@
+import { useMediaQuery } from "@/hooks/use-media-query";
 import { ModeToggle } from "../ModeToggle/modetoggle";
 import "./navbar.css";
+import { Hamburger } from "../MiniDropdown/hamburger";
 
-export default function menubar() {
+export default function Navbar() {
+  const isMobile = useMediaQuery("(max-width: 680px)");
+
   return (
     <div className="navbar-container">
       <h1 className="logo">𝔽𝔸𝕄</h1>
-      <ul className="nav-list">
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact</li>
-        <li>
-          <ModeToggle />
-        </li>
-      </ul>
+      <div>
+        {isMobile ? (
+          <ul className="nav-list">
+            <li>
+              <ModeToggle />
+            </li>
+            <li>
+              <Hamburger />
+            </li>
+          </ul>
+        ) : (
+          <ul className="nav-list">
+            <li>About</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact</li>
+            <li>
+              <ModeToggle />
+            </li>
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
