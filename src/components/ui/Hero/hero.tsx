a074123ebce5ea020e3df42c361d8fd424adc232
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-07-21 13:16:48 -0500

    hero's responsiveness done

--- src/components/ui/Hero/hero.tsx
+++ src/components/ui/Hero/hero.tsx
@@ -1,14 +1,22 @@
 import Image from "next/image";
 import "./hero.css";
 import { Button } from "../Button/button";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 export default function Hero() {
+  const isMobile = useMediaQuery("(max-width: 490px)");
+
   return (
     <div className="hero-container">
       <div className="heroText">
         <h4>Hello All! I am</h4>
         <h1>Frank Mensah </h1>
-        <h3 className="highlight">FullStack Developer and AI Enthusiast </h3>
+        {isMobile ? (
+          <h3 className="highlight">FullStack Developer</h3>
+        ) : (
+          <h3 className="highlight">FullStack Developer and AI Enthusiast </h3>
+        )}
+
         <h4>
           A Full-Stack Software Engineer based in Chicago, IL with expertise in{" "}
           <b className="highlight">C++</b>,{" "}
